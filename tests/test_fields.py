14256dd53ca6f8521c8017b6a49285f2b94eca26
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-05-18 21:58:44 +0530

    Fix DateTime field usage in Tuple and List containers

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -169,6 +169,16 @@ class TestParentAndName:
         assert schema2.fields["foo"].key_field.root == schema2
         assert schema2.fields["foo"].value_field.root == schema2
 
+    def test_tuple_with_datetime_field(self):
+        class TestSchema(Schema):
+            times = fields.Tuple((fields.DateTime(), fields.DateTime()))
+
+        try:
+            s = TestSchema()
+            assert True
+        except AttributeError:
+            pytest.fail("Tuple field with DateTime fields raised an AttributeError unexpectedly.")
+
 
 class TestMetadata:
     @pytest.mark.parametrize("FieldClass", ALL_FIELDS)
